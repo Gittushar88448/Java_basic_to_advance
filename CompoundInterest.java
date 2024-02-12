183ab2e77bfc6b1b27bab3489d69aba28187ed8e
Tushar <kmrtushar01@gmail.com>
2024-02-13 00:21:12 +0530

    Adding compound Interest program

---
+++ CompoundInterest.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+// Write a program to calculate the compound interest
+
+public class CompoundInterest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Principal amount: ");
+        int amt = input.nextInt();
+
+        System.out.print("Enter the rate of interest: ");
+        int rate = input.nextInt();
+
+        System.out.print("Enter the time in year: ");
+        int time = input.nextInt();
+
+        float calc = amt*(1+(float)rate/100)*time;
+
+        System.out.println("Compound interest of given information is : "+calc);
+
+    }
+}
