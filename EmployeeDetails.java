8fc5440b0ee2131f63098d82131b50eb78a9326a
Tushar <kmrtushar01@gmail.com>
2024-03-11 21:12:22 +0530

    employee class details using object

---
+++ EmployeeDetails.java
@@ -0,0 +1,12 @@
+public class EmployeeDetails {
+
+    public static void main(String[] args) {
+        Employee newEmp = new Employee("Rahul" , 32 , 32000);
+        System.out.println(newEmp.displayEmployeeDetails());
+
+        Employee newEmp1 = new Employee("JaiKishore" , 20 , 40000);
+        System.out.println(newEmp1.displayEmployeeDetails());
+
+    }
+
+}
