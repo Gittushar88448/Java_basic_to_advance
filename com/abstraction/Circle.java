ec70c9483b15956054edbb6862ccd7f314cc15b9
Tushar <kmrtushar01@gmail.com>
2024-03-19 23:25:09 +0530

    inherit the property of abstract Shape class and define its method

---
+++ com/abstraction/Circle.java
@@ -0,0 +1,24 @@
+package com.abstraction;
+
+public class Circle extends Shape{
+
+    public double radii;
+
+    public double getRadii() {
+        return radii;
+    }
+
+    public Circle(double radii) {
+        this.radii = radii;
+    }
+
+    public void setRadii(double radii) {
+        this.radii = radii;
+    }
+
+    @Override
+    public double calculateArea() {
+
+        return (2 * Math.PI * radii);
+    }
+}
