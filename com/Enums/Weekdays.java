e78ec9d060b1e806f47c9c705bbf18e29426b975
Tushar <kmrtushar01@gmail.com>
2024-04-25 18:22:00 +0530

    function which tells whether the day is weekday or weekend

--- com/Enums/Weekdays.java
+++ com/Enums/Weekdays.java
@@ -1,11 +1,29 @@
 package com.Enums;
 
 public enum Weekdays {
-    MONDAY , TUESDAY, WEDNESDAY , THURSDAY , FRIDAY, SATURDAY , SUNDAY;
+    MONDAY(true) ,
+    TUESDAY(true),
+    WEDNESDAY(true),
+    THURSDAY(true),
+    FRIDAY(true),
+    SATURDAY(false),
+    SUNDAY(false);
+
+    private final boolean isWeekday;
+
+    Weekdays(boolean isWeekday) {
+        this.isWeekday = isWeekday;
+    }
+
+    public String isWeekday(){
+      return   isWeekday ? "Weekday" : "Weekend";
+
+    }
 
     public static void main(String[] args) {
-        for(Weekdays days : Weekdays.values()){
-            System.out.println(days);
+        System.out.println("Printing day with its weekday or not");
+        for (Weekdays days : Weekdays.values()) {
+            System.out.printf("%s : %s \n", days ,days.isWeekday());
         }
     }
 
