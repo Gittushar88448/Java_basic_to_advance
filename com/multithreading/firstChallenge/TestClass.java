c1349385a7010e82b6a1898f79055a341f0a12a3
Tushar <kmrtushar01@gmail.com>
2024-06-01 21:20:10 +0530

    test class to test the HelloThread class

---
+++ com/multithreading/firstChallenge/TestClass.java
@@ -0,0 +1,10 @@
+package com.multithreading.firstChallenge;
+
+public class TestClass {
+    public static void main(String[] args) {
+        HelloThread t1 = new HelloThread(1);
+        HelloThread t2 = new HelloThread(2);
+        t1.start();
+        t2.start();
+    }
+}
